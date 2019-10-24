09cbd3e0ae27cc5c43cfa08377781c7c86457104
Ali <aliseylaneh@live.com>
2019-10-24 21:30:07 +0330

    Project Initialization and Adding required dependencies.(previous commit bug is fixed)

---
+++ src/main/java/com/aliseylaneh/simplejokeappspring5/SimpleJokeAppSpring5Application.java
@@ -0,0 +1,13 @@
+package com.aliseylaneh.simplejokeappspring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJokeAppSpring5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleJokeAppSpring5Application.class, args);
+    }
+
+}
